8daa2239774720c964725eb4288f51c4c62adbdb
Won Kim <wonsae.kim@gmail.com>
2019-04-03 17:34:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,6 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma
-  
+def speak_to_grandma(phrase)
+  if phrase.upcase?
+    
+  else
+    return "HUH?! SPEAK UP, SONNY"
+  end
 end
